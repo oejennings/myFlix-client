39670cb3ec3470e2edfbc98da89551b697bb6e73
Olivia Jennings <oejennings@users.noreply.github.com>
2023-08-28 12:03:17 -0400

    used useState to store list of movies

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
+
 export const MainView = () => {
+   const [movies, setMovies] = useState ([
+    { id: 1, title: "Clue" },
+    { id: 2, title: "The Avengers" },
+    { id: 3, title: "Jurrasic Park"}
+   ]);
+   if (movies.length === 0) {
+    return <div>The List is empty!</div>
+   } else {
     return (
-        // <div>
-        //   <div>Eloquent JavaScript</div>
-        //   <div>Mastering JavaScript Functional Programming</div>
-        //   <div>JavaScript: The Good Parts</div>
-        //   <div>JavaScript: The Definitive Guide</div>
-        //   <div>The Road to React</div>
-        // </div>
-    );
+        <div>
+            {movies.map((movie) => {
+                return <div key={movie.id}>{movie.title}</div>
+            })}
+        </div>
+    )
+   }
   };
