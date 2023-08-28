1ae619e25f5b19943963efcf671edd099789eeee
Olivia Jennings <oejennings@users.noreply.github.com>
2023-08-28 12:41:32 -0400

    created MovieView component

---
+++ src/components/movie-view/movie-view.jsx
@@ -0,0 +1,21 @@
+export const MovieView = ({ movie }) => {
+    return (
+        <div>
+            <div>
+                <img src={movie.image}/>
+            </div>
+            <div>
+                <span>Title: </span>
+                <span>{movie.title}</span>
+            </div>
+            <div>
+                <span>Genre: </span>
+                <span>{movie.genre}</span>
+            </div>
+            <div>
+                <span>Director: </span>
+                <span>{movie.director}</span>
+            </div>
+        </div>
+    )
+}
