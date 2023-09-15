c66e5be655d6e5dc76819e2ddc8300fa6eda6ca0
Olivia Jennings <oejennings@users.noreply.github.com>
2023-09-15 11:42:18 -0400

    connected API to app

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,36 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-   const [movies, setMovies] = useState ([
-    {
-        id: 1,
-        title: "Clue",
-        image: "https://m.media-amazon.com/images/I/913DShsadYL._AC_UF894,1000_QL80_.jpg",
-        description: "Six guests are anonymously invited to a strange mansion for dinner, but after their host is killed, they must cooperate with the staff to identify the murderer as the bodies pile up.",
-        genre: "Mystery",
-        director: "Jonathan Lynn"
-    },
-    {
-        id: 2,
-        title: "The Avengers",
-        image: "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
-        description: "Earths mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
-        genre: "Action",
-        director: "Joss Whedon"
-    },
-    {
-        id: 3,
-        title: "Jurassic Park",
-        image: "https://www.imdb.com/title/tt0107290/mediaviewer/rm3913805824/?ref_=tt_ov_i",
-        description: "A pragmatic paleontologist touring an almost complete theme park on an island in Central America is tasked with protecting a couple of kids after a power failure causes the park's cloned dinosaurs to run loose.",
-        genre: "Action",
-        director: "Steven Spielberg"
-    }
-   ]);
-
+   const [movies, setMovies] = useState([]);
    const [selectedMovie, setSelectedMovie] = useState(null);
+
+    useEffect(() => {
+        fetch("https://oj-movies-0c0784fe26f8.herokuapp.com/movies")
+        .then((response) => response.json())
+        .then((data) => {
+            const moviesFromApi = data.map((movie) => {
+                return {
+                    _id: movie._id,
+                    Title: movie.Title,
+                    Description: movie.Description,
+                    Genre: {
+                        Name: movie.Genre.Name,
+                        Description: movie.Genre.Description,
+                    },
+                    Director: {
+                        Name: movie.Director.Name,
+                        Bio: movie.Director.Bio,
+                    },
+                    ImagePath: movie.ImagePath 
+                };
+            });
+            setMovies(moviesFromApi);
+        });
+    }, []);
    
    if (selectedMovie) {
     return (
@@ -45,7 +43,7 @@ export const MainView = () => {
         <div>
             {movies.map((movie) => (
                 <MovieCard 
-                    key={movie.id} 
+                    key={movie._id} 
                     movie={movie}
                     onMovieClick={(newSelectedMovie) => {
                         setSelectedMovie(newSelectedMovie);
