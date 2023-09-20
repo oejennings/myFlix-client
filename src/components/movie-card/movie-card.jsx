127eb22d6aecb73473b25628a62223631c5c8d63
Olivia Jennings <oejennings@users.noreply.github.com>
2023-09-20 17:23:20 -0400

    added addFavoriteMovie

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,9 +1,41 @@
 import React from "react";
+import {useState} from "react";
 import PropTypes from "prop-types";
 import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-export const MovieCard = ({ movie }) => {
+export const MovieCard = ({ movie, user, token, setUser }) => {
+    const [isFavorite, setIsFavorite] = useState(
+        user.FavoriteMovies.includes(movie._id)
+    );
+
+    const addFavoriteMovie = () => {
+        fetch (`https://oj-movies-0c0784fe26f8.herokuapp.com/users/${user.Username}/movies/${movie._id}`,
+        {
+            method: "POST",
+            headers: {Authorization: `Bearer ${token}` }
+        })
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                alert('Failed');
+                return false;
+            }
+        })
+        .then((user) => {
+            if (user) {
+                alert("Added to Favorites");
+                localStorage.setItem("user", JSON.stringify(user));
+                setUser(user);
+                setIsFavorite(true);
+            }
+        })
+        .catch((e) => {
+            alert(e);
+        });
+    };
+
     return (
         <Card className="h-100">
             <Card.Img variant="top" src={movie.ImagePath} />
@@ -13,6 +45,17 @@ export const MovieCard = ({ movie }) => {
                 <Link to={`/movies/${movie._id}`}>
                     <Button variant="primary">Details</Button>
                 </Link>
+            </Card.Body>
+            <Card.Body>
+                {isFavorite ? (
+                    <Button variant="primary" onClick={removeFavoriteMovie}>
+                        Remove From Favorites
+                    </Button>
+                ) : (
+                    <Button variant="primary" onClick={addFavoriteMovie}>
+                        Add to Favorites
+                    </Button>
+                )}
             </Card.Body>
         </Card>
     );
